7787604cd8dd9a3c6047f9ea44d906b1f592af6f
Edgar Parucho <e.parucho@outlook.com>
2024-09-18 12:26:42 -0400

    Adds functionality to display and hide the sidebar for features.

---
+++ script.js
@@ -0,0 +1,100 @@
+const featuresByProject = {
+  mantra: [
+    {
+      featureImgPath: "./assets/mantra/mantra_feature-1-min.jpg",
+      featureAlt: "Mantra's UI bar and line charts about the service on smartphone.",
+      featureTitle: "Efficacy + efficiency",
+      featureDescription: "Every UI component is designed to provide easy-to-read, relevant information. You have everything you need for effective decision-making at your fingertips.",
+    },
+    {
+      featureImgPath: "./assets/mantra/mantra_feature-2-min.jpg",
+      featureAlt: "Mantra's UI informative cards about service indicators on smartphone.",
+      featureTitle: "User-friendly",
+      featureDescription: "Grasp the service status at a glance. Minimize input errors through intuitive, step-by-step forms. Find information rapidly with powerful filters. Mantra aims to boost your work without adding complications.",
+    },
+    {
+      featureImgPath: "./assets/mantra/mantra_feature-3-min.jpg",
+      featureAlt: "Mantra's large menu of features on smartphone.",
+      featureTitle: "Full control",
+      featureDescription: "From clients, to products, users, etc. You decide who is able to manipulate sensitive information.",
+    },
+    {
+      featureImgPath: "./assets/mantra/mantra_feature-4-min.jpg",
+      featureAlt: "Mantra app running on smartphone and desktop PC.",
+      featureTitle: "Availability",
+      featureDescription: "No matter where you are or what device you use, you only need an internet connection and a web browser to access Mantra's full set of features.",
+    },
+  ],
+  wallance: [
+    {
+      featureImgPath: "./assets/wallance/wallance_feature-1-min.jpg",
+      featureAlt: "Wallance's UI on smartphone: fund cards with their independent status (budget).",
+      featureTitle: "Funds",
+      featureDescription: "In real life, you probably separate your savings from your bills and optional expenses. Wallance allows you to create Funds so you can implement your own strategies.",
+    },
+    {
+      featureImgPath: "./assets/wallance/wallance_feature-2-min.jpg",
+      featureAlt: "Wallance's UI on desktop PC: pie and line charts to measure movements by tag and month.",
+      featureTitle: "Tags",
+      featureDescription: "Tag your incomes and expenses so you can visually measure and be aware of where and how everything comes and goes.",
+    },
+    {
+      featureImgPath: "./assets/wallance/wallance_feature-3-min.jpg",
+      featureAlt: "Wallance's UI on smartphone: query form with multiple options.",
+      featureTitle: "Easy querying",
+      featureDescription: "Get any data fast and easily filtering by any property of the record through an intuitive form.",
+    },
+    {
+      featureImgPath: "./assets/wallance/wallance_feature-4-min.jpg",
+      featureAlt: "Wallance app running on tablet and smartphone.",
+      featureTitle: "Beyond the features",
+      featureDescription: "Wallance's features provide status, organization, knowledge and awareness of your movements, but the purpose is to help you to find the best way to manage your finance by yourself, through your own movements.",
+    },
+  ],
+};
+
+
+function showSidebar(projectName) {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.setAttribute('class', 'sidebar');
+  setSidebarContent(projectName);
+  hideBodyScrollbar();
+}
+
+function setSidebarContent(projectName) {
+  setSidebarTitle(projectName);
+  setSidebarCards(projectName);
+}
+
+function setSidebarTitle(projectName) {
+  const [sidebarTitle] = document.getElementById('sidebar').getElementsByClassName('sidebar__title');
+  sidebarTitle.textContent = projectName.toUpperCase();
+  sidebarTitle.scrollIntoView({ behavior: "smooth" });
+}
+
+function setSidebarCards(projectName) {
+  const cards = document.getElementById('sidebar').getElementsByClassName('card');
+  for (let i in featuresByProject[projectName]) {
+    const [img] = cards[i].getElementsByTagName('img');
+    const [featureTitle] = cards[i].getElementsByTagName("dt");
+    const [featureDescription] = cards[i].getElementsByTagName("dd");
+    img.setAttribute('src', featuresByProject[projectName][i].featureImgPath);
+    img.setAttribute('alt', featuresByProject[projectName][i].featureAlt);
+    featureTitle.textContent = featuresByProject[projectName][i].featureTitle;
+    featureDescription.textContent = featuresByProject[projectName][i].featureDescription;
+  }
+}
+
+function hideSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.setAttribute('class', 'sidebar sidebar_hidden');
+  showBodyScrollbar();
+}
+
+function hideBodyScrollbar() {
+  document.getElementsByTagName('body')[0].setAttribute('style', 'overflow: hidden;');
+}
+
+function showBodyScrollbar() {
+  document.getElementsByTagName('body')[0].removeAttribute('style');
+}
